f4538615677edff64fa1b29fca4f7aebc156feb9
Slim <sazwerling@gmail.com>
2021-06-10 10:59:02 -0500

    further refactoring

--- src/components/App.js
+++ src/components/App.js
@@ -22,10 +22,6 @@ const App = () => {
       setSelectedVideo(response.data.items[0]);
     };
 
-      const onVideoSelect = (video) => {
-         setSelectedVideo(video);
-      };
-
       return (
          <div className="ui container">
             <SearchBar onFormSubmit={onTermSubmit} />
@@ -35,7 +31,8 @@ const App = () => {
                      <VideoDetail video={selectedVideo} />
                   </div>
                   <div className="five wide column">
-                     <VideoList onVideoSelect={onVideoSelect} videos={videos} />
+                     <VideoList onVideoSelect={setSelectedVideo} videos={videos} />
+                     {/* above is equal to this: <VideoList onVideoSelect={(video) => setSelectedVideo(video)} etc.*/}
                   </div>
                </div>
             </div>
